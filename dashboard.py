54a101f3d194fc7bdfda527c3b64ec9530ef3ad1
juliano-CN <juliano.nagy@hotmail.com>
2025-09-18 16:28:54 -0300

    resert

---
+++ dashboard.py
@@ -0,0 +1,129 @@
+#%%
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import kagglehub
+import os
+#baixar os dados
+# Download latest version
+path = kagglehub.dataset_download("fronkongames/steam-games-dataset")
+
+print("Path to dataset files:", path)
+
+data = pd.read_csv(os.path.join(path, "games.csv"),header=None,skiprows=1) 
+
+data.columns = ['AppID', 'Name', 'Release date', 'Estimated owners', 'Peak CCU',
+       'Required age', 'Price', 'Discount','DLC count', 'About the game',
+       'Supported languages', 'Full audio languages', 'Reviews',
+       'Header image', 'Website', 'Support url', 'Support email', 'Windows',
+       'Mac', 'Linux', 'Metacritic score', 'Metacritic url', 'User score',
+       'Positive', 'Negative', 'Score rank', 'Achievements', 'Recommendations',
+       'Notes', 'Average playtime forever', 'Average playtime two weeks',
+       'Median playtime forever', 'Median playtime two weeks', 'Developers',
+       'Publishers', 'Categories', 'Genres', 'Tags', 'Screenshots', 'Movies']
+
+data.to_csv("games.csv",index_label= False)
+
+#python -m streamlit run dashboard.py
+
+#configura layout da pagina
+st.set_page_config(layout="wide",page_title="Dashboard")
+
+#ler os dados
+data = pd.read_csv("games.csv",sep=",")
+data_copy = data.copy()#copia df
+data_copy = data_copy.drop_duplicates()#copia linhas repetidas
+#remove colunas que não vamos usar
+data_copy = data_copy.drop(['AppID','Discount','About the game','Peak CCU',
+        'Reviews','Header image', 'Website', 'Support url', 
+        'Support email','Metacritic url','Notes',
+        'Score rank', 'Recommendations',
+        'Average playtime two weeks','Median playtime two weeks',
+        'Screenshots', 'Movies'],axis=1)
+
+data_copy = data_copy[data_copy["Estimated owners"] != "0 - 0"]
+data_copy = data_copy[~data_copy['Genres'].isna()]
+data_copy['Genres'] = data_copy['Genres'].str.lower().str.strip()
+
+#transformar variaveis
+#converte para data
+data_copy['Release date'] = data_copy['Release date'].astype("datetime64[ns]")
+
+#título
+st.title("Dashborad Steam")
+
+#texto
+introduction, = st.columns(1, border=True)  # vírgula desempacota a lista
+introduction.write("Dados dos jogos da steam coletados aproximadamente em abril de 2025. Altere os valores " \
+"para atualizar os gráficos. Mais informações leia o README.md.")
+
+#barra-------
+# Pega a data mínima e máxima do dataframe
+min_date = data_copy['Release date'].dt.year.min()
+max_date = data_copy['Release date'].dt.year.max()
+
+# Cria um slider de intervalo de datas
+date_range = st.slider(
+    "Período:",
+    min_value=min_date,
+    max_value=max_date,
+    value=(min_date, max_date)# valor inicial (intervalo completo)
+)
+
+# escolher o gênero
+Genres = data_copy['Genres'].str.lower().dropna()
+Genres = Genres.str.strip("[]").str.split(",")
+
+uniques = Genres.explode().str.strip().drop_duplicates().tolist()
+
+g = st.multiselect("Gênero(s)", uniques)
+
+# checkbox-------
+freeGames = st.checkbox("Remover jogos grátis")
+
+#filtrar no dataFrame
+if freeGames:
+        df_Free = data_copy[data_copy['Price'] > 0]
+else:
+        df_Free = data_copy
+
+df_genres = df_Free[df_Free['Genres'].apply(
+                                lambda x:all(item in [x.strip().lower() for x in x.split(",")]
+                                for item in g))]
+
+df_filtered = df_genres[(df_genres["Release date"].dt.year>=date_range[0]) &
+               (df_genres["Release date"].dt.year<=date_range[1])]
+
+#texto
+col0, = st.columns(1, border=True)
+col0.write(f"{len(df_filtered)} jogos encontrados")
+
+# graficos/análise-------
+col1,col2 = st.columns(2,border=True)
+
+if len(df_filtered) != 0:
+        freq_year = df_filtered['Release date'].dt.year.value_counts().sort_index(ascending=True)
+        fig_date1 = px.line(freq_year,y= freq_year.values,x = freq_year.index,
+                    title = "Quantidades de lançamentos por ano")#,labels={"y": "","Lançamento":""}
+        col1.plotly_chart(fig_date1)
+
+
+        freq_mounth = df_filtered['Release date'].dt.month.value_counts()
+        fig_date2 = px.bar(freq_mounth,y= freq_mounth.values,x = freq_mounth.index,
+                   title = "Quantidades de lançamentos por mês")
+        col2.plotly_chart(fig_date2)
+
+
+col3,col4 = st.columns(2,border=True)
+
+if len(df_filtered) != 0:
+        fig_date3 = px.box(df_filtered,y = 'Price',
+                           title='distribuição de preços',
+                           hover_data=["Name"])
+        col3.plotly_chart(fig_date3)
+
+        fig_date4 = px.scatter(df_filtered,x="Negative",y="Positive",color="Estimated owners",
+                               title= "Votos positivos x negativos",
+                               hover_data=["Name","Required age","Price"])
+
+        col4.plotly_chart(fig_date4)
